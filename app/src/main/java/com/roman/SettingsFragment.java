2d05320c82c5db2fe614fb192045ec880a9d0b43
romandmowski <roman.dmowski@outlook.com>
2020-03-24 19:43:20 -0400

    add dynamic update of appRolePreference summary

--- app/src/main/java/com/roman/SettingsFragment.java
+++ app/src/main/java/com/roman/SettingsFragment.java
@@ -1,8 +1,12 @@
 package com.roman;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import android.view.Menu;
 
 import com.roman.pawelm.R;
 
@@ -12,5 +16,36 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+
+        ListPreference appRolePreference = (ListPreference) findPreference("app_role");
+
+
+        if (appRolePreference != null) {
+
+            if (appRolePreference.getValue().equals(getString(R.string.app_role_camera_key))) {
+                appRolePreference.setSummary(R.string.app_role_camera_label);
+            } else {
+                appRolePreference.setSummary(R.string.app_role_display_label);
+            }
+
+            appRolePreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object new_app_role) {
+
+                    String app_role = (String) new_app_role;
+
+                    if (app_role.equals(getString(R.string.app_role_camera_key))) {
+                        preference.setSummary(R.string.app_role_camera_label);
+                    } else {
+                        preference.setSummary(R.string.app_role_display_label);
+                    }
+                    return true;
+                }
+            });
+
+
+        }
     }
+
 }
