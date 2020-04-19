c7cd25f887218e01034dda6310715789b5c5b2f3
romandmowski <roman.dmowski@outlook.com>
2020-04-19 13:28:45 -0400

    save login and password to SharedPreferences inLoginActivity

--- app/src/main/java/com/roman/LoginActivity.java
+++ app/src/main/java/com/roman/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.roman;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,23 +26,83 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginUser = findViewById(R.id.new_user);
-        loginPassword = findViewById(R.id.new_password);
-        loginButton=findViewById(R.id.register_button);
-        textViewLinkSignUp=findViewById(R.id.textViewLinkSignUp);
+        loginUser = findViewById(R.id.login_user);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_button);
+        textViewLinkSignUp = findViewById(R.id.textViewLinkSignUp);
 
-//        loginButton.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                // Code here executes on main thread after user presses button
-//                createNewLogin("login647","passw345");
-//            }
-//        });
-//
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                saveLogPass();
+                finish();
+            }
+        });
+
         textViewLinkSignUp.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent intent = new Intent(LoginActivity.this, NewUserActivity.class);
                 startActivity(intent);
             }
         });
+
+
+
+        loginUser.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                enableLoginButton();
+            }
+        });
+
+        loginPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                enableLoginButton();
+            }
+        });
+
+
+    }
+
+
+    void enableLoginButton()
+    {
+        if ((loginUser.length()>3) && (loginPassword.length()>3))
+        {
+            loginButton.setEnabled(true);
+        }
+        else
+        {
+            loginButton.setEnabled(false);
+        }
+    }
+
+    private void saveLogPass()
+    {
+        SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        SharedPreferences.Editor editor = SP.edit();
+        editor.putString(getString(R.string.preferences_user_key),loginUser.toString());
+        editor.putString(getString(R.string.preferences_pass_key),loginPassword.toString());
+        editor.apply();
     }
 }
