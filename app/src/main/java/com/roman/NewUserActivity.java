79aa3285bc095d65400612cadd975f3d214fda91
romandmowski <roman.dmowski@outlook.com>
2020-04-20 20:44:37 -0400

    new user activity scripts

--- app/src/main/java/com/roman/NewUserActivity.java
+++ app/src/main/java/com/roman/NewUserActivity.java
@@ -3,6 +3,8 @@ package com.roman;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,7 +38,15 @@ public class NewUserActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
-                createNewLogin("login647","passw345");
+                //createNewLogin("login647","passw345");
+
+                Intent intent = new Intent(NewUserActivity.this, MainActivity.class);
+                intent.putExtra("_newLogin", newLogin.getText().toString());
+                intent.putExtra("_newPass", newPassword.getText().toString());
+                startActivity(intent);
+
+
+
             }
         });
 
@@ -47,31 +57,103 @@ public class NewUserActivity extends AppCompatActivity {
             }
         });
 
+
+
+        newLogin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                enableRegisterButton();
+            }
+        });
+
+        newPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                enableRegisterButton();
+            }
+        });
+
+
+        newPasswordConfirmation.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                enableRegisterButton();
+            }
+        });
+
+
+
+
+
     }
 
 
 
-    private void createNewLogin(String _newUser, String _newPass)
+//    private void createNewLogin(String _newUser, String _newPass)
+//    {
+//
+//       //MainActivity.createNewUser(_newUser,_newPass);
+//
+//
+////        try {
+////            JSONObject json = new JSONObject();
+////
+////            json.put("type", "newuser");
+////            json.put("newlogin", "test001");
+////            json.put("newpass", "password02");
+////
+////            //wsListener.send(json.toString());
+////            Logging.d(TAG, "Creating newuser: " + json.toString());
+////        } catch (JSONException e) {
+////            e.printStackTrace();
+////        }
+//
+//
+//
+
+//    }
+
+
+    void enableRegisterButton()
     {
 
-       //MainActivity.createNewUser(_newUser,_newPass);
-
-
-//        try {
-//            JSONObject json = new JSONObject();
-//
-//            json.put("type", "newuser");
-//            json.put("newlogin", "test001");
-//            json.put("newpass", "password02");
-//
-//            //wsListener.send(json.toString());
-//            Logging.d(TAG, "Creating newuser: " + json.toString());
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-
-
-
-
+        if ((newLogin.length() >3) && (newPassword.length()>3) && (newPassword.getText().toString().equals(newPasswordConfirmation.getText().toString())))
+        {
+            registerButton.setEnabled(true);
+        }
+        else
+        {
+            registerButton.setEnabled(false);
+        }
     }
 }
